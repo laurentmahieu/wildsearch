1167a5ab04c605f1993f0349c9d281639e6dca82
LaurentMahieu <mahieu.l@hotmail.fr>
2019-09-25 17:36:42 +0200

    js mobile btn top OK

--- JS/laurent.js
+++ JS/laurent.js
@@ -1,3 +1,5 @@
+//DESKTOP
+
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
@@ -5,5 +7,16 @@ function myFunction() {
     document.getElementById("upPageDesktop").style.opacity = "0.8";
   } else {
     document.getElementById("upPageDesktop").style.opacity = "0";
+  }
+}
+// MOBILE 
+
+window.onscroll = function() {myFunctionMobile()};
+
+function myFunctionMobile() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("upPageMobile").style.opacity = "0.6";
+  } else {
+    document.getElementById("upPageMobile").style.opacity = "0";
   }
 }
