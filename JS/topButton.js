a6987ddf320c2ccd5113c53786f129300ec840ad
LaurentMahieu <mahieu.l@hotmail.fr>
2019-09-26 09:20:14 +0200

    finaly the topPage btn work on every page

--- JS/topButton.js
+++ JS/topButton.js
@@ -4,19 +4,9 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("upPageDesktop").style.opacity = "0.8";
+    document.getElementById("upPage").style.opacity = "0.8";
   } else {
-    document.getElementById("upPageDesktop").style.opacity = "0";
+    document.getElementById("upPage").style.opacity = "0";
   }
 }
 // MOBILE 
-
-window.onscroll = function() {myFunctionMobile()};
-
-function myFunctionMobile() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("upPageMobile").style.opacity = "0.6";
-  } else {
-    document.getElementById("upPageMobile").style.opacity = "0";
-  }
-}
