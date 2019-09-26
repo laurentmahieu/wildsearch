908ee56d4f15bb0e019c2255818b7620ea77f0df
Clemence Caux <clemencecaux@gmail.com>
2019-09-26 08:55:39 +0200

    creation databasee def

---
+++ database-def.php
@@ -0,0 +1,149 @@
+<?php
+$alphabet = [
+   'A' => [
+   ],
+    'B' => [
+    ],
+    'C' => [
+        ['CMV',
+         'link',
+         'content management system (CMS) manages the creation and modification of digital content...'],
+        ['CODEWARS',
+         'link',
+          'an educational community for computer programming. On the platform, software developers train on programming challenges known as Kata....'
+        ],
+        ['CSS',
+            'link',
+            'stands for Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML..., software developers train on programming challenges known as Kata....'
+        ],
+    ],
+    'D' => [
+        ['Database',
+         'link',
+         'collection of information that is organized so that it can be easily accessed, managed and updated...',
+        ],
+        ['Developer',
+            'link',
+            'programmer who specializes in the development of applications relating to the World Wide Web or distributed network applications...',
+        ],
+    ],
+    'E' => [
+
+    ],
+    'F' => [
+
+    ],
+    'G' => [
+
+    ],
+    'H' => [
+        ['HTML',
+        'link',
+        'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser...',
+        ],
+    ],
+    'I' => [
+        ['Integration',
+         'link',
+         'a process of connecting all the outputs of all activities and components essential for carrying out the web project as a whole...',
+        ],
+    ],
+    'J' => [
+
+        ['JavaScript',
+            'link',
+            'one of the core technologies of the World Wide Web. JavaScript enables interactive web pages and is an essential part of web applications...',
+        ],
+    ],
+    'K' => [
+
+    ],
+    'L' => [
+
+    ],
+    'M' => [
+        ['MVC',
+         'link',
+         'stands for Model, View and Controller. MVC separates application into three components - Model, View and Controller...',
+        ],
+    ],
+    'N' => [
+        ['NodeJS',
+         'link',
+         'an open-source, cross-platform, JavaScript run-time environment that executes JavaScript code outside of a browser...',
+        ],
+    ],
+    'O' => [
+
+    ],
+    'P' => [
+        ['Product Owner',
+         'link',
+         'the leader responsible for maximizing the value of the products created by a scrum development team....',
+        ],
+    ],
+    'Q' => [
+
+    ],
+    'R' => [
+        ['React',
+         'link',
+         'a JavaScript library for building user interfaces. It is maintained by Facebook and...',
+        ],
+        ['React Native',
+            'link',
+            'React Native is a JavaScript framework for writing real, natively rendering mobile applications for iOS and Android...',
+        ],
+    ],
+    'S' => [
+        ['Scrum',
+            'link',
+            'a framework within which people can address complex adaptive problems, while productively and creatively delivering products...'],
+        ['Scrum Master',
+            'link',
+            'the person responsible for ensuring Scrum is understood and enacted...',
+        ],
+        ['SQL',
+            'link',
+            'a standard language for storing, manipulating and retrieving data in databases...',
+        ],
+    ],
+    'T' => [
+
+    ],
+    'U' => [
+        ['Unix',
+            'link',
+            'a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix...',
+        ],
+        ['Ux-Design',
+            'link',
+            'User experience design (UX design) is the process of enhancing user satisfaction with by improving the usability, accessibility, and desirability...',
+        ],
+    ],
+    'V' => [
+
+    ],
+    'W' => [
+        ['W3C',
+            'link',
+            'stands World Wide Web Consortium (W3C) is the main international standards organization...',
+        ],
+        ['Wireframe',
+            'link',
+            'a visual guide that represents the skeletal framework of a website...',
+        ],
+    ],
+    'X' => [
+
+    ],
+    'Y' => [
+
+    ],
+    'Z' => [
+
+    ],
+
+];
+
+var_dump($alphabet);
